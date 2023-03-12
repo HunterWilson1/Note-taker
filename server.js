8ffa2fb82c68f3659ce3a03de6e4c19525aafc34
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-12 13:00:36 -0500

    I maded the routes for html and api and started working on the js to deploy server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const api = require('./routes/api')
+const html = require('./routes/html')
+
+app.use(express.static('public'));
+
+app.use('/api', api)
+app.use('/', html)
+
+ app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.listen(PORT, () => console.log(`App server hosted on ${PORT}`))
