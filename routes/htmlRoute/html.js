d7901ae953beb18b64cdc16ea8e8a0ca28d3b95d
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-14 21:12:03 -0500

    I made more js files but I am not really sure where to go with this

--- routes/htmlRoute/html.js
+++ routes/htmlRoute/html.js
@@ -1,15 +1,14 @@
-
-const path = require('path')
-const router = require('express').router
+const path = require("path");
+const router = require("express").router;
 
 //first route is defined at root and sends the index when GET request is sent
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'))
-})
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
 
 //second route is at notes endpoint so when a get request is made to /notes this html is sent
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'))
-})
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/notes.html"));
+});
 
-module.exports = router
+module.exports = router;
