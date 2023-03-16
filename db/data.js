d3c231fc08e4cea695c85b49d8ed14599bddab13
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-16 15:26:58 -0500

    I added a delete notes method and worked on the addNotes method but it is not working

--- db/data.js
+++ db/data.js
@@ -8,12 +8,16 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 class Data {
+    //reads content of db.json
     read() {
         return readFile("db/db.json", "utf8" )
     }
+
+    //Write the contents of note to db.json
     write(note) {
         return writeFile("db/db.json", JSON.stringify(note))
     }
+    
     getNotes() {
         return this.read().then((notes) => {
             let blip ;
@@ -24,6 +28,28 @@ class Data {
             }
             return blip;
         })
+    }
+    
+
+    addNote(note) {
+        const {title, text} = note;
+        
+        //new object with id
+        const newNote = {title, text, id: uuidv4()};
+
+        return this.getNotes()
+            //gets all notes from db.json, concates existing notes with new note and saves it,
+            //and returns new note object
+            .then((notes) => [...notes, newNote])
+            .then(savedNotes => this.write(savedNotes))
+            .then(() => newNote)
+    }
+
+    //deletes not with given id from db.json
+    delNote(id) {
+        return this.getNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(filNotes  => this.write(filNotes))
     }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -1,38 +1,25 @@
-const router = require('express').Router()
-const fs = require('fs');
-const data = require('../db/data')
+const router = require("express").Router();
+
+const data = require("../db/data");
 //This does a get request from /notes and reads data from the db.json using the fs
 //It then sends the JSON data back to user
 router.get("/notes", (req, res) => {
-    data
+  data
     .getNotes()
     .then((notes) => {
-        return res.json(notes)
+      res.json(notes);
     })
-    .catch((err) => res.status(500).json(err))
+    .catch((err) => res.status(500).json(err));
 });
-
-
 
 //router endpooint
 router.post("/notes", (req, res) => {
-    //reads what is in db.json
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-
-        //parses the data from db.json
-        const newNote = JSON.parse(data)
-        //appends the new note content
-        newNote.push(req.body);
-
-        //writes the updated newNote array to the db.json file
-        
+  data
+    .addNote(req.body)
+    .then((note) => {
+      res.json(note);
     })
-
-    //ends response
-    res.end
-})
-
+    .catch((err) => res.status(500).json(err));
+});
 
 module.exports = router;
-
