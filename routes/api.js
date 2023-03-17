3a7a4a746f67440a6de19bc5b577dcf86df0769b
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-16 20:59:43 -0500

    I started reworking the code based off of previous methods I had made

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,33 @@
 const router = require("express").Router();
-const data = require("../db/data");
+const path = require('path');
+const fs = require('fs');
+const {v4: uuidv4} = require('uuid');
+const noteDb = require('../db/db.json')
 
-//This does a get request from /notes and reads data from the db.json using the fs
-//It then sends the JSON data back to user
+
 router.get("/notes", (req, res) => {
-  data
-    .getNotes()
-    .then((notes) => {
       res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
 });
 
-//router endpooint
+/* //router endpooint
 router.post("/notes", (req, res) => {
   data
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
 });
-
+ */
 router.delete("/notes/:id", (req, res) => {
-  data
-    .delNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
+  for (let i = 0; i <  note.length; i++) {
+    if (noteDb[i].id === req.params.id) {
+      noteDb.splice(i, 1)
+    }
+  }
+  fs.writeFile(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify(noteDb, null, 2)
+  )
+  res.json(noteDb)
 });
+
 module.exports = router;
